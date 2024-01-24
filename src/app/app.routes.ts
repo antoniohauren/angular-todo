6483f5ca5514584fcafdd3385a9678fd27c83ca8
Antonio Hauren <antoniohauren@protonmail.com>
2024-01-24 11:55:12 -0300

    chore: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { TodoComponent } from './pages/todo/todo.component';
+import { DefaultComponent } from './shared/layouts/default/default.component';
+import { MainComponent } from './shared/layouts/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [{ path: '', component: LoginComponent }],
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [{ path: 'todo', component: TodoComponent }],
+  },
+];
