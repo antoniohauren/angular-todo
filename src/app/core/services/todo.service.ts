d32c1d7234e64f145e604c7c18d24e67d1135b3b
Antonio Hauren <antoniohauren@protonmail.com>
2024-01-24 14:29:31 -0300

    chore: fake data todos

--- src/app/core/services/todo.service.ts
+++ src/app/core/services/todo.service.ts
@@ -8,9 +8,31 @@ export class TodoService {
   private todos: Todo[] = [
     {
       id: 1,
-      title: 'first todo',
-      description: 'blah',
+      title: 'OPEN',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quam optio perferendis, quidem fugit ducimus sapiente ea totam nam accusantium nulla at, tenetur dolore maiores facilis iure. Natus, autem eligendi.',
       status: 'OPEN',
+    },
+    {
+      id: 2,
+      title: 'PROGRESS',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quam optio perferendis, quidem fugit ducimus sapiente ea totam nam accusantium nulla at, tenetur dolore maiores facilis iure. Natus, autem eligendi.',
+      status: 'PROGRESS',
+    },
+    {
+      id: 3,
+      title: 'TESTING',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quam optio perferendis, quidem fugit ducimus sapiente ea totam nam accusantium nulla at, tenetur dolore maiores facilis iure. Natus, autem eligendi.',
+      status: 'TESTING',
+    },
+    {
+      id: 4,
+      title: 'DONE',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quam optio perferendis, quidem fugit ducimus sapiente ea totam nam accusantium nulla at, tenetur dolore maiores facilis iure. Natus, autem eligendi.',
+      status: 'DONE',
     },
   ];
 
